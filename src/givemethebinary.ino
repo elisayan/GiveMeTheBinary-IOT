a63fc5b939ade2cc7f2410b727425f865eb4e7dd
Elisa Yan <elisa.yan@studio.unibo.it>
2024-10-11 20:46:10 +0200

    initialized ino

---
+++ src/givemethebinary.ino
@@ -0,0 +1,14 @@
+/*
+ * Assignment #1 - Give Me the Binary! (GMB)
+ * Author: Elisa Yan
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
